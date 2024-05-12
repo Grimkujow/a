a0300b3ed9c7e4234b306c5d3ae7ddb7b7b5b23a
Nicolas Lagrange <130107205+Grimkujow@users.noreply.github.com>
2024-05-12 18:24:33 +0200

    Delete TDEESTINEE directory

--- TDEESTINEE/Projet_tinder/sidebar/sidebar_identifier/connexion.php
+++
@@ -1,7 +0,0 @@
-<form id="emailForm" method="post">
-    <label for="email">Adresse e-mail :</label><br><br>
-    <input type="email" id="email" name="email" placeholder="Entrez votre adresse e-mail" required><br>
-    <small>Exemple : votreadresse@example.com</small><br><br>
-
-    <input type="submit" value="Continuer">
-</form>
